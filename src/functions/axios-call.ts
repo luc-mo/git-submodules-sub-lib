8fdd2734d9ff1d9870fe65d17f1c1461b229bb24
SnowDrive <luc.moralesr@gmail.com>
2025-07-13 17:29:33 -0400

    feat: update main function name

--- src/functions/axios-call.ts
+++ src/functions/axios-call.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const main = async() => {
+export const axiosCall = async() => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
   return response.data
 }
